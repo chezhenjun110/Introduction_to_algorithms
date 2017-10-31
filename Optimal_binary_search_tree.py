9fdd9dbcf0b30eb51f44ff2f3a60e7f736d31927
chezhenjun110 <740102367@qq.com>
2017-10-31 12:02:06 +0800

    x

--- Optimal_binary_search_tree.py
+++ Optimal_binary_search_tree.py
@@ -1,15 +1,21 @@
 p=[0,0.15,0.10,0.05,0.10,0.20] #关键字
 q=[0.05,0.10,0.05,0.05,0.05,0.10] #伪关键字
-e=[]
-w=[]
-root=[]
 n=len(p)
+e=[[0 for i in range(n)] for j in range(n+1)]   #期望代价
+w=[[0 for i in range(n)] for j in range(n+1)]
+root=[[0 for i in range(n)] for j in range(n)]
 for i in range(1,n+1):
-    for j in range(n):
-        e[i][j]=0
-        w[i][j]=0
-print(len(e))
-# for i in range(1,n+1):
+    e[i][i-1]=q[i-1]
+    w[i][i-1]=q[i-1]
+for l in range(1,n+1):
+    for i in range(1,n-l+1):
+        j=i+l-1
+        e[i][j]=1000
+        w[i][j]= w[i][j-1]+p[j]+q[j]
+        for r in range(i,j):
+            t=e[i][r-1]+e[r+1][j]+w[i][j]
+            if t<e[i][j]:
+                e[i][j]=t
+                root[i][j]=r
+print(e)
 
-    # e[i][i-1]=q[i-1]
-    # w[i][i-1]=q[i-1]
